36c50e930c6846d648eef3eb16c9b456f40e5b04
Aaron Wang <154568367+aaronrwang@users.noreply.github.com>
2025-01-24 11:02:59 -0500

    Update all.py for delete

--- webscrape/all.py
+++ webscrape/all.py
@@ -3,6 +3,7 @@ import board
 import solve
 import mongodb
 import refresh
+from datetime import datetime, timedelta
 
 try:
     tango = board.filter(scrape.scrape())
@@ -12,6 +13,10 @@ try:
     solvedGrid, moves = solve.solve(tango)
     data['moves'] = moves
     mongodb.insert(data, "SolvedBoards")
+
+    date_15_days_ago = str((datetime.now() - timedelta(days=15)).date())
+    mongodb.delete(date_15_days_ago)
+
     refresh.refresh()
 except Exception as e:
     print(f"Attempt failed: {e}")
